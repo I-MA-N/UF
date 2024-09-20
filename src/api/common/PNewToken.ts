2b54addcb5559e51f9103c9689f834a18e4ff00b
Iman Mostafavi <imn.s901386@gmail.com>
2024-09-20 20:15:05 +0330

    Fix refresh token bug

--- src/api/common/PNewToken.ts
+++ src/api/common/PNewToken.ts
@@ -1,15 +1,29 @@
-import axios from "axios";
 import Cookies from "js-cookie";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import getFormData from "../../utils/getFormData";
 
 function PNewToken() {
+   const queryClient = useQueryClient();
+
    const { mutateAsync } = useMutation({
       mutationKey: ['get: access token'],
       mutationFn: async () => {
          const rTkn = Cookies.get('refresh');
-         const req = await axios.post(`${import.meta.env.VITE_ENDPOINT}/login/refresh/`, { refresh: rTkn });
 
-         return req.data as { access: string }
+         const req = await fetch(`${import.meta.env.VITE_ENDPOINT}/login/refresh/`, {
+            method: "POST",
+            body: getFormData({ refresh: rTkn })
+         })
+
+         const data = await req.json();
+
+         return data;
+      },
+      onSettled: (data) => {
+         if (!data?.access) {
+            Cookies.remove('refresh');
+            queryClient.clear();
+         }
       }
    })
 
